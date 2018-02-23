f9471ef6e63fcf6000598571cb6d0f8d1eb16362
Larry Lu <pudding850806@gmail.com>
2018-02-23 10:24:28 +0800

    Add eslint example

---
+++ index.js
@@ -0,0 +1,24 @@
+const a = {
+  type: 'ExpressionStatement',
+  start: 0,
+  end: 5,
+  expression: {
+    type: 'AssignmentExpression',
+    start: 0,
+    end: 5,
+    operator: '=',
+    left: {
+      type: 'Identifier',
+      start: 0,
+      end: 1,
+      name: 'a',
+    },
+    right: {
+      type: 'Literal',
+      start: 4,
+      end: 5,
+      value: 1,
+      raw: '1',
+    },
+  },
+}
